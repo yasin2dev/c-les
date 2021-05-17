5156d06c04ecb33df4e0b743413c4e367db71dc4
yasin2dev <yasin.bozkurt.n@gmail.com>
2021-05-17 17:33:44 +0300

    add comments

--- example-1/calculator.c
+++ example-1/calculator.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 
 int main() {
+	//define operator and numbers
 	char operator;
 	int first, second;
 	printf("Please enter an operator (+, -, *, /): ");
+	//Read operator
 	scanf("%c", &operator);
 	printf("Please enter two operands: ");
+	//Read first and second number
 	scanf("%d %d", &first, &second);
 
 	switch(operator) {
 		case '+':
+		    //If operator +
 		    printf("%d + %d = %d\n", first, second, first + second);
 		    break;
 		case '-':
+		    //If operator -
 		    printf("%d - %d = %d\n", first, second, first - second);
 		    break;
 		case '*': 
+		    //If operator *
 		    printf("%d * %d = %d\n", first, second, first * second);
 		    break;
 		case '/':
+		    //If operator /
 		    printf("%d / %d = %d\n", first, second, first / second);
 		    break;
 		default:
+		    //If operator not * / + -
 		    printf("Error! operator is not correct!");
 	}
 	return 0;
-}		
+}	
